7ba0c3d3ff6e22f765aa509bc49ef7da1a94fa70
Khalid <khalid.sanggoyod@fligno.com>
2025-07-21 21:27:24 +0800

    [frontend - feat]: added navbar UI with login and signup text

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Section from './Section';
 
 const logo = "/nav-logo.png";
 
@@ -8,18 +9,44 @@ const Header: React.FC = () => {
   return (
     <header className="fixed top-0 left-0 z-20 w-full">
       <nav className="ease-in-ou fixed top-0 left-0 z-20 w-full transition-all duration-300">
-        <div className="flex h-full w-full items-center justify-center">
-          <div className="flex w-[90%] xl:w-[96%] max-w-screen-2xl flex-col gap-6 xl:flex-row">
-            <div className="flex w-full flex-wrap items-center justify-between py-4">
+        <Section>
+          <div className="flex w-full flex-wrap items-center justify-between py-6">
+            <div className="flex items-center space-x-26">
               <a
                 href="/"
                 className="flex items-center space-x-3 rtl:space-x-reverse"
               >
                 <img src={logo} alt="" className="w-40" />
               </a>
+              <ul className="flex items-center space-x-6">
+                <li>
+                  <a className='text-white' href="/">Overview</a>
+                </li>
+                <li>
+                  <a className='text-white' href="/about">Features</a>
+                </li>
+                <li>
+                  <a className='text-white' href="/contact">Pricing</a>
+                </li>
+                <li>
+                  <a className='text-white' href="/contact">About</a>
+                </li>
+              </ul>
+            </div>
+            <div className="flex items-center space-x-6">
+              <ul className="flex items-center space-x-6">
+                <li>
+                  <a className='text-white' href="/login">Log In</a>
+                </li>
+                <li>
+                  <div className="flex py-2 px-4 border-2 border-white rounded-full">
+                    <a className='text-[#FFCC00]' href="/register">Sign Up</a>
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
-        </div>
+        </Section>
       </nav>
     </header>
   );
--- frontend/src/components/Section.tsx
+++ frontend/src/components/Section.tsx
@@ -12,7 +12,7 @@ const Section: React.FC<SectionProps> = ({ children }) => {
             className='flex h-full w-full items-center justify-center'
         >
             <div
-                className="flex w-[92%] xl:w-[84%] max-w-screen-2xl flex-col gap-6 md:flex-row"
+                className="flex w-[92%] xl:w-[90%] max-w-screen-2xl flex-col gap-6 md:flex-row"
             >
                 {children}
             </div>
