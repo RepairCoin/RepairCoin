8ed95f045a514f53c5c78232b76fa426077c93ba
Khalid <khalid.sanggoyod@fligno.com>
2025-07-21 22:16:18 +0800

    [frontend - feat]: adjustment margin

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -16,15 +16,15 @@ export const Hero: React.FC<HeroProps> = ({
       style={backgroundImage ? { backgroundImage: `url(${backgroundImage})` } : {}}
     >
       <Section>
-        <div className='flex flex-col mb-36'>
-          <div className='w-3/7 py-2 px-4'>
+        <div className='flex flex-col border-2 border-white mb-36'>
+          <div className='w-3/7 border-2 border-white py-2'>
             <p className='text-[#FFCC00] text-lg mb-6'>THE REPAIR INDUSTRY ‘S LOYALTY TOKEN</p>
             <p className='text-5xl font-bold text-white mb-4'>Reward your Repairs with RepairCoin</p>
             <p className='text-white text-base mb-6'>
               Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
             </p>
           </div>
-          <div className='flex flex-row gap-6 pt-20'>
+          <div className='flex flex-row gap-6 border-2 border-white pt-20'>
             <button className='bg-[#FFCC00] text-black py-4 px-6 rounded-full font-semibold text-lg text-center'>Get Started <span className='ml-2 text-base'>→</span></button>
             <button className='border border-white text-white py-4 px-6 rounded-full font-semibold text-lg text-center'>Find a Participating Repair Shop <span className='ml-2 text-base'>→</span></button>
           </div>
