064f67f52b34e2465ebae80b58516917ab421f16
ChiefTechDev <jerome.pangilinan914@gmail.com>
2025-09-17 20:21:07 -0400

    connect backend api on customer

---
+++ mobile/services/ShopServices.ts
@@ -0,0 +1,37 @@
+const API_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.132.85:3000/api';
+
+export const listShops = async () => {
+  const response = await fetch(`${API_URL}/shops`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  return response;
+}
+
+export const getShopById = async (shopId: string) => {
+  const response = await fetch(`${API_URL}/shops/${shopId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  return response;
+}
+
+export const getShopByWalletAddress = async (address: string) => {
+  const response = await fetch(`${API_URL}/shops/wallet/${address}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+
+  return response;
+}
