fe4fb9c0886a114b0a6f818d5ee5f27c60884c7c
ChiefTechDev <jerome.pangilinan914@gmail.com>
2025-09-23 14:15:34 -0400

    draft customer dashboard

--- mobile/app/dashboard/customer/index.tsx
+++ mobile/app/dashboard/customer/index.tsx
@@ -1,23 +1,25 @@
 import PrimaryButton from "@/components/PrimaryButton";
 import Screen from "@/components/Screen";
 import { Entypo } from "@expo/vector-icons";
-import { Image, View, Text } from "react-native";
+import { Image, View, Text, Pressable } from "react-native";
 import { useAuthStore } from "@/store/authStore";
+import { useState } from "react";
+
+type CustomerTabs = "Wallet" | "Referral" | "Approval";
 
 export default function CustomerDashboard() {
   const { logout } = useAuthStore((state) => state);
+  const [activeTab, setActiveTab] = useState<CustomerTabs>("Wallet");
+  const customerTabs: CustomerTabs[] = ["Wallet", "Referral", "Approval"];
 
   return (
     <Screen>
       <View className="pt-14 px-6">
-        <View className="flex-row justify-between items-center">
-          <Image
-            source={require("@/assets/images/logo.png")}
-            className="w-[40%] h-10"
-            resizeMode="contain"
-          />
-          <Entypo name="help-with-circle" color="#fff" size={24} />
-        </View>
+        <Image
+          source={require("@/assets/images/logo.png")}
+          className="w-[40%] h-10"
+          resizeMode="contain"
+        />
         <View className="flex-row my-4">
           <Text className="text-lg font-semibold text-[#FFCC00] mr-2">
             Hello!
@@ -25,21 +27,28 @@ export default function CustomerDashboard() {
           <Text className="text-lg font-semibold text-white">John Doe!</Text>
         </View>
         <View className="flex-row w-full h-12 bg-[#121212] rounded-xl justify-between">
-          <View className="w-[33%] bg-[#FFCC00] my-1 ml-[0.5%] rounded-xl items-center justify-center">
-            <Text className="text-lg font-semibold text-black">Wallet</Text>
-          </View>
-          <View className="w-[33%] bg-[#121212] my-1 rounded-xl items-center justify-center border-r-[#4B4B4B]">
-            <Text className="text-lg font-semibold text-[#4B4B4B]">
-              Referrals
-            </Text>
-          </View>
-          <View className="w-[33%] bg-[#121212] my-1 mr-[0.5%] rounded-xl items-center justify-center">
-            <Text className="text-lg font-semibold text-[#4B4B4B]">
-              Approvals
-            </Text>
-          </View>
+          {customerTabs.map((tab, i) => (
+            <>
+              <Pressable
+                onPress={() => {
+                  activeTab !== tab && setActiveTab(tab);
+                }}
+                key={i}
+                className={`bg-${activeTab === tab ? "[#FFCC00]" : "[#121212]"} w-[33%] flex-row ${i === 0 && "rounded-l-xl"} ${i === 2 && "rounded-r-xl"} items-center justify-center`}
+              >
+                <Text
+                  className={`text-lg font-bold text-${activeTab === tab ? "black" : "gray-400"}`}
+                >
+                  {tab}
+                </Text>
+              </Pressable>
+              {i !== 2 && activeTab === customerTabs[2 - 2 * i] && (
+                <View className="w-[0.1%] bg-gray-400 my-2" />
+              )}
+            </>
+          ))}
         </View>
-        <PrimaryButton title="Logout" onPress={logout} />
+        {/* <PrimaryButton title="Logout" onPress={logout} /> */}
       </View>
     </Screen>
   );
