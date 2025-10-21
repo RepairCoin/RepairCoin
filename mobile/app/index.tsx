d6479b71e52ec7574f42cc954059e6cf4cbc0bde
Khalid Sanggoyod <sanggoyodk8@gmail.com>
2025-10-21 23:02:21 +0800

    fix: remove logo from index and trasferred to splashscreen file

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,7 +1,5 @@
 import SplashScreen from "@/screen/splash-screen/SplashScreen";
 
-const logo = require("@/assets/images/logo.png");
-
 export default function Splash() {
   return (
     <SplashScreen />
