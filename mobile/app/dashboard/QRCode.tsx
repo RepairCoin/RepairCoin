85675022f898aa0447817e6c404f5a5cfa4d5286
Khalid Sanggoyod <sanggoyodk8@gmail.com>
2025-10-28 20:56:32 +0800

    fix: [Home Tab > Approval Tab] - UI adjustment still pending

--- mobile/app/dashboard/QRCode.tsx
+++ mobile/app/dashboard/QRCode.tsx
@@ -1,7 +1,6 @@
 import { AntDesign } from "@expo/vector-icons";
 import { goBack } from "expo-router/build/global-state/routing";
 import { View, Text, Image, Pressable } from "react-native";
-import Screen from "@/components/ui/Screen";
 import { useState } from "react";
 import PrimaryButton from "@/components/ui/PrimaryButton";
 import ShareQRCodeModal from "@/components/customer/ShareQRCodeModal";
@@ -12,32 +11,13 @@ export default function QRCode() {
 
   return (
     <View className="w-full h-full px-4 bg-white">
-      <View className="h-20" />
       <View className="mx-2 flex-row justify-between items-center">
         <AntDesign name="left" color="black" size={25} onPress={goBack} />
         <Text className="text-black text-[22px] font-extrabold">QR Code</Text>
         <View className="w-[25px]" />
       </View>
 
-      <View className="flex-row mt-8">
-        <View className="flex-1">
-          <Text className="text-black/25 text-2xl text-center">Scan QR</Text>
-          <View className="bg-black/25 h-0.5 w-full mt-2" />
-        </View>
-        <View className="flex-1">
-          <Text className="text-black text-2xl text-center">My QR</Text>
-          <View className="bg-black h-0.5 w-full mt-2" />
-        </View>
-      </View>
-
-      <Pressable
-        onPress={() => router.push("/dashboard/SendRCNAmountInputPage")}
-      >
-        <Image
-          source={require("@/assets/images/qrcode.png")}
-          className="mt-16"
-        />
-      </Pressable>
+      <Image source={require("@/assets/images/qrcode.png")} className="mt-16 border-2" />
 
       <Text className="text-black text-3xl text-center font-extrabold mt-2">
         Your QR Code
