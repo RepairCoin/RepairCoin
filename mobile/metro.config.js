f99f261d29097fc0c35b9eabb9968803b3f571dc
Khalid Sanggoyod <sanggoyodk8@gmail.com>
2025-10-31 10:01:08 +0800

    fix: fixing build apk | pending issue

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -5,6 +5,11 @@ const config = getDefaultConfig(__dirname);
 
 config.resolver.extraNodeModules = {
   "react-async-hook": require.resolve("react-async-hook"),
+  '@aws-sdk/client-kms': require.resolve('./shims/emptyModule.js'),
+  '@aws-sdk/credential-providers': require.resolve('./shims/emptyModule.js'),
+  '@aws-sdk/client-lambda': require.resolve('./shims/emptyModule.js'),
+  'react-native-quick-crypto': require.resolve('./shims/emptyModule.js'),
+  'react-native-aes-gcm-crypto': require.resolve('./shims/emptyModule.js'),
 };
 
 module.exports = withNativeWind(config, { input: "./global.css" });
