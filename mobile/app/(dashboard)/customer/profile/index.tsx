5deda56d57d876f4081d34cde4c59bfe2f1e24c4
Khalid Sanggoyod <sanggoyodk8@gmail.com>
2025-11-03 20:10:10 +0800

    fix: [Profile Tab] - UI adjustment and added dynamic customer data

--- mobile/app/(dashboard)/customer/profile/index.tsx
+++ mobile/app/(dashboard)/customer/profile/index.tsx
@@ -10,6 +10,8 @@ import React, { useEffect, useState } from "react";
 import * as Clipboard from "expo-clipboard";
 import { router } from "expo-router";
 import { useAuthStore } from "@/store/authStore";
+import { useCustomerStore } from "@/store/customerStore";
+import { useCustomer } from "@/hooks";
 
 type CopyableFieldProps = {
   value: string;
@@ -26,17 +28,17 @@ const CopyableField = ({
     onPress={handleCopyValue}
     className={`p-4 ${
       isCopied ? "bg-[#FFCC00] justify-center" : "border-dashed justify-between"
-    } border-2 border-[#FFCC00] flex-row  rounded-xl`}
+    } border-2 border-[#FFCC00] flex-row rounded-xl`}
   >
     {isCopied ? (
-      <Text className="text-xl text-white font-semibold">
+      <Text className="text-base text-white font-semibold">
         <Entypo name="check" color="#fff" size={18} />
         {"  "}Code copied to clipboard
       </Text>
     ) : (
       <React.Fragment>
-        <Text className="text-xl text-[#FFCC00] font-semibold">{value}</Text>
-        <Text className="text-lg text-[#ffcc00a2] font-semibold">
+        <Text className="text-base text-[#FFCC00] font-semibold">{value}</Text>
+        <Text className="text-base text-[#ffcc00a2] font-semibold">
           Tap to copy
         </Text>
       </React.Fragment>
@@ -45,14 +47,24 @@ const CopyableField = ({
 );
 
 export default function MyProfile() {
-  const [isCopied, setIsCopied] = useState<boolean>(false);
-  const [isNotificationEnabled, setIsNotificationEnabled] =
-    useState<boolean>(false);
   const { logout } = useAuthStore((state) => state);
+  const { account } = useAuthStore();
+  // Use the token balance hook
+  const {
+    data: customerData,
+  } = useCustomer(account?.address);
+
+  console.log("customerData: ", customerData?.customer)
+
+  const [isCopied, setIsCopied] = useState<boolean>(false);
 
   const handleCopyValue = async (value: string) => {
     await Clipboard.setStringAsync(value);
     setIsCopied(true);
+  };
+
+  const handleLogout = async () => {
+    await logout();
   };
 
   useEffect(() => {
@@ -65,37 +77,25 @@ export default function MyProfile() {
     }
   }, [isCopied]);
 
-  const toggleNotification = () => {
-    setIsNotificationEnabled(!isNotificationEnabled);
-  };
-
-  const handleLogout = async () => {
-    // Logout will clear auth state, disconnect wallet, clear storage, and navigate to onboarding
-    await logout();
-  }
-
   return (
     <View className="w-full h-full bg-zinc-950 px-4 pt-24">
       <ScrollView>
         <View className="flex-row py-6 px-4 justify-between bg-[#212121] rounded-xl items-center">
           <View className="gap-2">
-            <Text className="text-[#FFCC00] text-3xl font-bold">John Doe</Text>
-            <Text className="text-white/50 text-lg">johndoe@gmail.com</Text>
+            <Text className="text-[#FFCC00] text-xl font-bold">{customerData?.customer.name}</Text>
+            <Text className="text-white/50 text-base">{customerData?.customer.email}</Text>
           </View>
           <Pressable
             onPress={() => router.push("/EditProfile")}
-            className="bg-white p-2 max-h-12 w-28 rounded-lg flex-row justify-center items-center"
+            className="bg-white p-2 max-h-12 w-24 rounded-lg flex-row justify-center items-center"
           >
-            <Feather name="edit" color="#000" size={16} />
-            <Text className="text-black ml-2 text-lg">Edit</Text>
+            <Feather name="edit" color="#000" size={14} />
+            <Text className="text-black ml-2 text-base">Edit</Text>
           </Pressable>
         </View>
         <View className="py-6 px-4 bg-[#212121] rounded-xl gap-4 mt-4">
           <View className="flex-row justify-between items-center">
-            <Text className="text-white/50 text-2xl">Wallet Address</Text>
-            <Pressable className="w-10 h-10 rounded-full bg-yellow-400 justify-center items-center">
-              <MaterialIcons name="qr-code-scanner" size={20} color="black" />
-            </Pressable>
+            <Text className="text-white/50 text-xl">Wallet Address</Text>
           </View>
           <CopyableField
             value="0xab....057912345"
@@ -103,10 +103,13 @@ export default function MyProfile() {
             isCopied={isCopied}
           />
         </View>
-        <View className="py-6 px-4 bg-[#212121] rounded-xl gap-4 mt-4">
-          <Pressable onPress={handleLogout} className="flex-row justify-between items-center mt-4">
+        <View className="p-4 bg-[#212121] rounded-xl mt-4">
+          <Pressable
+            onPress={handleLogout}
+            className="flex-row justify-between items-center"
+          >
             <View className="flex-row items-center">
-              <View className="rounded-full bg-[#FBCDCD] w-16 h-16 items-center justify-center">
+              <View className="rounded-full bg-[#FBCDCD] w-12 h-12 items-center justify-center">
                 <MaterialIcons name="logout" color="#E74C4C" size={24} />
               </View>
               <View className="px-4 gap-2">
