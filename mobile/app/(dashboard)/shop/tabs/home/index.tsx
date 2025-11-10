2223eaddd812de4428ca07c73f58e72390fe9015
Khalid Sanggoyod <sanggoyodk8@gmail.com>
2025-11-10 15:29:29 +0800

    feat: [Shop Dashboard > Home Tab] - Implement shop data store with zustand and still working UI

--- mobile/app/(dashboard)/shop/tabs/home/index.tsx
+++ mobile/app/(dashboard)/shop/tabs/home/index.tsx
@@ -1,8 +1,23 @@
-import { Image, Pressable, View } from "react-native";
+import React from "react";
+import { Image, Pressable, Text, View } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { router } from "expo-router";
+import { useShopByWalletAddress } from "@/hooks";
+import { useAuthStore } from "@/store/authStore";
+
+type ShopTabs = "Wallet" | "Analysis" | "Promo Code";
 
 export default function Home() {
+  const { account } = useAuthStore();
+  const { data, isLoading, error } = useShopByWalletAddress(
+    account?.address || ""
+  );
+
+  const [activeTab, setActiveTab] = React.useState<ShopTabs>("Wallet");
+  const shopTabs: ShopTabs[] = ["Wallet", "Analysis", "Promo Code"];
+
+  console.log("SHOPPP DATAA: ", data);
+
   return (
     <View className="h-full w-full bg-zinc-950">
       <View className="h-full w-full pt-14 px-4">
@@ -18,6 +33,37 @@ export default function Home() {
           >
             <Feather name="bell" size={20} color="white" />
           </Pressable>
+        </View>
+        <View className="flex-row my-4 justify-between items-center">
+          <View className="flex-row">
+            <Text className="text-lg font-semibold text-[#FFCC00] mr-2">
+              Hello!
+            </Text>
+            <Text className="text-lg font-semibold text-white">
+              {data?.data.name}
+            </Text>
+          </View>
+        </View>
+        <View className="flex-row w-full h-10 bg-[#121212] rounded-xl justify-between">
+          {shopTabs.map((tab, i) => (
+            <React.Fragment key={i}>
+              <Pressable
+                onPress={() => {
+                  activeTab !== tab && setActiveTab(tab);
+                }}
+                className={`bg-${activeTab === tab ? "[#FFCC00]" : "[#121212]"} w-[33%] flex-row ${i === 0 && "rounded-l-xl"} ${i === 2 && "rounded-r-xl"} items-center justify-center`}
+              >
+                <Text
+                  className={`text-base font-bold text-${activeTab === tab ? "black" : "gray-400"}`}
+                >
+                  {tab}
+                </Text>
+              </Pressable>
+              {i !== 2 && activeTab === shopTabs[2 - 2 * i] && (
+                <View className="w-[0.1%] bg-gray-400 my-2" />
+              )}
+            </React.Fragment>
+          ))}
         </View>
       </View>
     </View>
--- mobile/hooks/useShopQueries.ts
+++ mobile/hooks/useShopQueries.ts
@@ -1,6 +1,11 @@
-import { useQuery, useQueryClient } from '@tanstack/react-query';
-import { queryKeys } from '@/config/queryClient';
-import { ShopResponse, listShops } from '@/services/ShopServices';
+import { useQuery } from "@tanstack/react-query";
+import { queryKeys } from "@/config/queryClient";
+import {
+  ShopResponse,
+  listShops,
+  getShopByWalletAddress,
+  ShopByWalletAddressResponse,
+} from "@/services/ShopServices";
 
 export const useShops = () => {
   return useQuery({
@@ -8,6 +13,18 @@ export const useShops = () => {
     queryFn: async () => {
       const response: ShopResponse = await listShops();
       return response.data;
+    },
+    staleTime: 10 * 60 * 1000, // 10 minutes
+  });
+};
+
+export const useShopByWalletAddress = (walletAddress: string) => {
+  return useQuery({
+    queryKey: queryKeys.shopByWalletAddress(walletAddress),
+    queryFn: async () => {
+      const response: ShopByWalletAddressResponse = await getShopByWalletAddress(walletAddress);
+      console.log("SHOPP RESPONSEE: ", response)
+      return response;
     },
     staleTime: 10 * 60 * 1000, // 10 minutes
   });
