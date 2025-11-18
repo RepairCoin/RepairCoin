62d66b4c58ace6735a3f716487609d4c82554c85
Khalid Sanggoyod <sanggoyodk8@gmail.com>
2025-11-18 17:23:54 +0800

    feat: added HorizontalCard reusable component

--- mobile/app/(dashboard)/shop/tabs/customer/index.tsx
+++ mobile/app/(dashboard)/shop/tabs/customer/index.tsx
@@ -1,17 +1,60 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { ThemedView } from "@/components/ui/ThemedView";
+import {
+  Feather,
+  Fontisto,
+  MaterialCommunityIcons,
+  Octicons,
+} from "@expo/vector-icons";
+import { LinearGradient } from "expo-linear-gradient";
+import HorizontalCard from "@/components/ui/HorizontalCard";
+import React from "react";
 
 export default function CustomerList() {
+  const horizontalCardList: {
+    label: string;
+    Icon: any;
+    number: number;
+  }[] = [
+    {
+      label: "Total Referrals",
+      Icon: <Octicons name="people" color="#ffcc00" size={22} />,
+      number: 12,
+    },
+    {
+      label: "RCN Earned",
+      Icon: (
+        <MaterialCommunityIcons
+          name="hand-coin-outline"
+          color="#ffcc00"
+          size={22}
+        />
+      ),
+      number: 12,
+    },
+    {
+      label: "Pending\nReferrals",
+      Icon: <Fontisto name="clock" color="#ffcc00" size={22} />,
+      number: 12,
+    },
+    {
+      label: "Successful\nReferrals",
+      Icon: <Feather name="user-check" color="#ffcc00" size={22} />,
+      number: 12,
+    },
+  ];
+
   return (
     <ThemedView className="w-full h-full">
-      <View className="pt-20 px-4 gap-4">
-        <View className="flex-row justify-between items-center">
-          <Text className="text-white text-xl font-semibold">
-            Customer List
-          </Text>
-          <View className="w-[25px]" />
+      <ScrollView className="mb-[144px]">
+        <View className="flex-row flex-wrap my-4 -mx-2">
+          {horizontalCardList.map((props, i) => (
+            <View key={i} style={{ width: "50%" }}>
+              <HorizontalCard {...props} />
+            </View>
+          ))}
         </View>
-      </View>
+      </ScrollView>
     </ThemedView>
   );
 }
---
+++ mobile/components/ui/HorizontalCard.tsx
@@ -0,0 +1,40 @@
+import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+export default function HorizontalCard({
+  label,
+  Icon,
+  number,
+}: {
+  label: string;
+  Icon: any;
+  number: number;
+}) {
+  return (
+    <View className="flex-1 h-28 rounded-2xl overflow-hidden m-2">
+      <LinearGradient
+        colors={["#373737", "#121212"]}
+        start={{ x: 1, y: 0 }}
+        end={{ x: 0, y: 1 }}
+        className="flex-1 h-full p-4 relative"
+      >
+        <View
+          className="w-48 h-48 border-[#141414] border-[40px] rounded-full absolute"
+          style={{
+            top: -25,
+            left: 100,
+          }}
+        />
+        <View className="flex-col justify-center px-2 h-full">
+          <View className="flex-row items-center justify-between w-full">
+            <Text className="text-[#FFCC00] text-sm font-bold">{label}</Text>
+            {Icon}
+          </View>
+          <Text className="text-white text-2xl mt-2 font-semibold">
+            {number}
+          </Text>
+        </View>
+      </LinearGradient>
+    </View>
+  );
+}
