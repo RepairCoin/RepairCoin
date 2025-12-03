6d9df1ecfa121f9ed27af4adb51a3e6f48a30dcd
Khalid Sanggoyod <sanggoyodk8@gmail.com>
2025-12-03 18:31:56 +0800

    feat: booking feature implementation on shop

--- mobile/hooks/booking/useBooking.ts
+++ mobile/hooks/booking/useBooking.ts
@@ -1,7 +1,7 @@
 import { bookingApi } from "@/services/booking.services";
 import { useQuery } from "@tanstack/react-query";
 import { queryKeys } from "@/config/queryClient";
-import { BookingFilters } from "@/interfaces/booking.interfaces";
+import { BookingFilters, BookingResponse } from "@/interfaces/booking.interfaces";
 
 export function useBooking() {
   const useShopBookingQuery = (
@@ -10,7 +10,7 @@ export function useBooking() {
     return useQuery({
       queryKey: queryKeys.bookings(filters),
       queryFn: async () => {
-        const response: any = await bookingApi.getShopBookings(filters);
+        const response: BookingResponse = await bookingApi.getShopBookings(filters);
         return response.data;
       },
       staleTime: 5 * 60 * 1000, // 5 minutes
@@ -23,7 +23,7 @@ export function useBooking() {
     return useQuery({
       queryKey: queryKeys.bookings(filters),
       queryFn: async () => {
-        const response: any = await bookingApi.getCustomerBookings(filters);
+        const response: BookingResponse = await bookingApi.getCustomerBookings(filters);
         return response.data;
       },
       staleTime: 5 * 60 * 1000, // 5 minutes
